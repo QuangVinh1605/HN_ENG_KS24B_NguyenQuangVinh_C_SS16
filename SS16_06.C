de77c0c92ea89ddccc8c390a4c0846af3ba13923
QuangVinh1605 <nhv16052006@gmail.com>
2024-12-15 23:43:28 +0700

    upload baitap ss16-06

---
+++ SS16_06.C
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int searchElement(int *arr, int size, int value) {
+    for (int i = 0; i < size; i++) {
+        if (arr[i] == value) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int main() {
+    int arr[] = {1, 2, 4, 5, 6, 7};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int value = 7;
+    printf("cac phan tu tron mang: ");
+	for (int i=0;i<size;i++){
+		printf("%d ",arr[i]); 
+	}
+	printf("\n") ;
+	printf("phan tu co gia tri la %d nam o vi tri: ",value);
+
+    int result = searchElement(arr, size, value);
+
+    printf("%d\n", result);
+
+    return 0;
+}
+
