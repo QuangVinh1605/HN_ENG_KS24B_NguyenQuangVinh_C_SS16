45a1ef8c67ac35d090680ed282e6f4e9076c48fc
QuangVinh1605 <nhv16052006@gmail.com>
2024-12-15 23:44:19 +0700

    upload baitap ss16-07

---
+++ SS16_07.C
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+void sortArray(int *arr, int size) {
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = i + 1; j < size; j++) {
+            if (arr[i] > arr[j]) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+}
+
+int main() {
+    int arr[] = {2, 3, 9, 1, 6, 7};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    sortArray(arr, size);
+
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
+
